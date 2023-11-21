8837e98c389155cbd99535fd8d58c2551c53aa86
Sumit Hotchandani <sumitjh@uw.edu>
2023-11-20 22:00:38 -0800

    Time: 21 ms (13.56%) | Memory: 41.3 MB (70.22%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -9,37 +9,18 @@ class Solution {
         Map<Character, Character> hashMap = new HashMap<>();
         // Map<Character, Integer> tMap = new HashMap<>();
 
-        // populate hashmap with character as key and count of occurrence for each string
         for( int i = 0; i < s.length(); i++ ) {
-            // if key exists increment count
+            // if key exists and current character from other string doesn't match existing entry : return false
             if ( hashMap.containsKey(s.charAt(i)) && hashMap.get(s.charAt(i)) != t.charAt(i) ) {
                return false; 
             } else if ( !hashMap.containsKey(s.charAt(i)) && hashMap.containsValue(t.charAt(i))) {
+                // if key doesn't exist but value's mapping with other key already exists : return false
                 return false;
             } else {
                 // if key is missing add new entry
                 hashMap.put( s.charAt(i), t.charAt(i) );
             }
         }
-
-        // for( int i = 0; i < t.length(); i++ ) {
-        //     if ( tMap.containsKey(t.charAt(i)) ) {
-        //         tMap.put( t.charAt(i), tMap.get(t.charAt(i)) + 1 );
-        //     } else {
-        //         tMap.put( t.charAt(i), 1 );
-        //     }
-        // }
-
-        // Iterator<Map.Entry<Character, Integer>> sIterator = sMap.entrySet().iterator();
-        // Iterator<Map.Entry<Character, Integer>> tIterator = tMap.entrySet().iterator();
-
-        // while(sIterator.hasNext() && tIterator.hasNext()) {
-        //     Map.Entry<Character, Integer> sEntry = sIterator.next();
-        //     Map.Entry<Character, Integer> tEntry = tIterator.next();
-        //     if( sEntry.getValue() != tEntry.getValue() ) 
-        //         return false;
-        // }
-
 
         return true;
     }
