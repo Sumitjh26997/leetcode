a3be13ce895d69d922e546d8b46bf9e95ef5a468
Sumit Hotchandani <sumitjh@uw.edu>
2023-12-27 19:30:13 -0800

    Time: 4 ms (15.54%) | Memory: 45.9 MB (8.71%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+       int i = 0, count = 0;
+       for(int j = 0; j < nums.length; j++) {
+           if(nums[j] != 0){
+               nums[i] = nums[j];
+               i++;
+           } else {
+               count++;
+           }
+       } 
+       System.out.println(count);
+       for(int j = nums.length - 1; j >= 0; j--) {
+           if(j == (nums.length - 1 - count)) {
+               break;
+           }
+           nums[j] = 0;
+       }
+    }
+}
