12b53f013042e19c455ecbb103c56a1d3850f5e8
Sumit Hotchandani <sumitjh@uw.edu>
2023-12-27 19:48:14 -0800

    Time: 11 ms (5.83%) | Memory: 42.7 MB (6.10%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i,j;
+        for(i = 0; i < matrix.length - 1; i++) {
+            if(target >= matrix[i][0] && target < matrix[i+1][0]) {
+                break;
+            }
+        }
+        
+        System.out.println("row found: " + i);
+        for(j = 0; j < matrix[0].length; j++) {
+            if(target == matrix[i][j])
+                return true;
+        }
+        
+        return false;
+    }
+}
