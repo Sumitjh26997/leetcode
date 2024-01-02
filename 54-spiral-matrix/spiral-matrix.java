a814d5016d3a5689c4c45564d958b3780ecde79c
Sumit Hotchandani <sumitjh@uw.edu>
2024-01-02 12:17:18 -0800

    Time: 0 ms (100.00%) | Memory: 41.5 MB (7.18%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -14,19 +14,15 @@ class Solution {
             
             //left to right
             for(int i = left; i <= right; i++) {
-                System.out.print(matrix[top][i] + " ");
                 traversal.add(matrix[top][i]);
             }
-            System.out.println();
 
             top++;
 
             //top to bottom
             for(int j = top; j <= bottom; j++) {
-                System.out.print(matrix[j][right] + " ");
                 traversal.add(matrix[j][right]);
             }
-            System.out.println();
 
             right--;
 
@@ -34,10 +30,8 @@ class Solution {
             if(top <= bottom){
                 
                 for(int i = right; i >= left; i--) {
-                    System.out.print(matrix[bottom][i] + " ");
                     traversal.add(matrix[bottom][i]);
                 }
-                System.out.println();
 
                 bottom--;
             }
@@ -46,16 +40,11 @@ class Solution {
             if(left <= right) {
                 //bottom to top
                 for(int j = bottom; j >= top; j--) {
-                    System.out.print(matrix[j][left] + " ");
                     traversal.add(matrix[j][left]);
                 }
-                System.out.println();
 
                 left++;
             }
-            
-
-            System.out.println("top: " + top + " left : " + left + " right: " + right + " bottom: " + bottom);
         }
         
         return traversal;
