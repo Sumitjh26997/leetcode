280344b5e3e3aa30a932ccaa71dd7d4d3e93b2b5
Sumit Hotchandani <sumitjh@uw.edu>
2024-01-05 17:00:04 -0800

    Time: 15 ms (5.87%) | Memory: 44.6 MB (5.20%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String common = strs[0];
+        System.out.print(common);
+
+        for(int i = 1; i < strs.length; i++) {
+            String comm="";
+            System.out.print(comm);
+            for(int j = 0; j < strs[i].length(); j++) {
+                if(common.length() == 0 || j >= common.length()) {
+                    break;
+                }
+                if(common.charAt(j) == strs[i].charAt(j)){
+                    comm+=common.charAt(j);
+                } else {
+                    break;
+                }
+            }
+            common = comm;
+        }
+
+        return common;
+    }
+}
