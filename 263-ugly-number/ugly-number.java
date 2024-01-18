ac4379ced84a124c7f26ce6bdd0461965be3d37e
Sumit Hotchandani <sumitjh@uw.edu>
2024-01-18 11:53:53 -0800

    Time: 1 ms (73.39%) | Memory: 40.9 MB (16.21%) - LeetSync

---
+++ 263-ugly-number/ugly-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n == 0) {
+            return false;
+        }
+        if(n == 1) {
+            return true;
+        }
+
+        int[] factors = {2, 3, 5};
+        for(int i = 0; i < factors.length; ) {
+            if(n % factors[i] == 0) {
+                n /= factors[i];
+            } else {
+                i++;
+            }
+        }
+
+        if(n == 1) {
+            return true;
+        }
+        return false;
+    }
+}
