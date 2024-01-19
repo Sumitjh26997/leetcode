74450308b2709c894d6a2b6beec1c3cececa277c
Sumit Hotchandani <sumitjh@uw.edu>
2024-01-18 23:09:55 -0800

    Time: 0 ms (100.00%) | Memory: 40.8 MB (38.20%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int addDigits(int num) {
+        if(num < 10) {
+            return num;
+        }
+        int result = 0;
+        
+        while(true) {
+            while(num > 0) {
+                result += num % 10;
+                num /= 10;
+            }
+            if(result >= 10) {
+                num = result;
+                result = 0;
+            } else {
+                break;
+            }
+        }
+        
+
+        return result;
+    }
+}
