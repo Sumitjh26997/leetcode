b06b3c4bb131a58c58488be9d89f91487fbe47fe
Sumit Hotchandani <sumitjh@uw.edu>
2024-01-19 12:56:39 -0800

    Time: 30 ms (5.21%) | Memory: 40.4 MB (20.10%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,4 +1,5 @@
 class Solution {
+    HashMap<Integer, Integer> memo = new HashMap<>();
     public int fib(int n) {
         if(n==0){
             return 0;
@@ -6,6 +7,11 @@ class Solution {
         if(n==1){
             return 1;
         }
-        return fib(n-2) + fib(n-1);
+
+        if(memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        int a = fib(n-2) + fib(n-1);
+        return a;
     }
 }
