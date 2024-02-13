0f5452dbbfb64c9e58692f7c07d0bd71bdafedd9
Sumit Hotchandani <sumitjh@uw.edu>
2024-02-13 14:08:22 -0800

    Time: 0 ms (100.00%) | Memory: 41.4 MB (7.56%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n == 0)
             return false;
+        if(n == 1)
+            return true;
+        if(n % 2 != 0)
+            return false;
         
-        while(n % 2 == 0) {
-            n /= 2;
-        }
-
-        return n == 1;
+        return isPowerOfTwo(n/2);
     }
 }
