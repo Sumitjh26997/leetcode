84ac6cc7c4a148817cf510b94642f2286a25a909
Sumit Hotchandani <sumitjh@uw.edu>
2024-02-19 20:56:49 -0800

    Time: 0 ms (100.00%) | Memory: 45.6 MB (92.45%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -5,7 +5,6 @@ class Solution {
         
         while(left <= right) {
             int mid = left + (right - left) / 2;
-            System.out.println("mid: " + mid);
             
             if(nums[mid] == target) {
                 result[0] = mid;
@@ -22,7 +21,6 @@ class Solution {
         
         while(left <= right) {
             int mid = left + (right - left) / 2;
-            System.out.println("mid: " + mid);
             
             if(nums[mid] == target) {
                 result[1] = mid;
