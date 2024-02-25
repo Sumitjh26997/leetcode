b5d077dc244c68af902bc7d7510695a037e51970
Sumit Hotchandani <sumitjh@uw.edu>
2024-02-25 04:37:23 -0800

    Time: 3 ms (24.43%) | Memory: 44.6 MB (47.71%) - LeetSync

---
+++ 1609-find-all-the-lonely-nodes/find-all-the-lonely-nodes.java
@@ -0,0 +1,46 @@
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getLonelyNodes(TreeNode root) {
+        List<Integer> lonely = new ArrayList<>();
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()) {
+            TreeNode curr = queue.poll();
+            if(curr.left == null && curr.right == null) {
+                continue;
+            }
+            if(curr.left != null && curr.right != null) {
+                queue.offer(curr.left);
+                queue.offer(curr.right);
+            }
+            if(curr.left == null) {
+                lonely.add(curr.right.val);
+                queue.offer(curr.right);
+            }
+            if(curr.right == null) {
+                lonely.add(curr.left.val);
+                queue.offer(curr.left);
+            }
+        }
+
+        return lonely;
+    }
+}
