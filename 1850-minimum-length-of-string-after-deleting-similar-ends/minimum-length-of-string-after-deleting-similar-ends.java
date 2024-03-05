afe6473f2ccb425777410f050c5dd1bf08af10c1
Sumit Hotchandani <sumitjh@uw.edu>
2024-03-04 22:13:49 -0800

    Time: 119 ms (5.08%) | Memory: 45.6 MB (7.30%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
@@ -3,12 +3,10 @@ class Solution {
         int left = 0, right = s.length() - 1;
         
         while(left < right) {
-            // System.out.println("1s: " + s);
             if(s.charAt(left) != s.charAt(right)) {
                 break;
             }
 
-            // System.out.println("2s: " + s); 
             char c = s.charAt(left);
             while(left <= right && c == s.charAt(left)) {
                 left++;
@@ -19,9 +17,7 @@ class Solution {
             }
 
 
-            // System.out.println("left : " + left + " right :" + right);
             s = s.substring(left, right+1);
-            // System.out.println("3s: " + s);
             left = 0;
             right = s.length() - 1;
         }
