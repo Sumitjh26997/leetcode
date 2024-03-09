ef269802c621fc9a9bb01d19dfd9d51b7d42d85d
Sumit Hotchandani <sumitjh@uw.edu>
2024-03-08 22:27:29 -0800

    Time: 18 ms (9.33%) | Memory: 63.7 MB (19.85%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,28 +1,17 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-       HashSet<Integer> n1 = new HashSet<>();
        HashSet<Integer> common = new HashSet<>();
 
        for(int num : nums1) {
-           n1.add(num);
+           common.add(num);
        }
 
        for(int num : nums2) {
-           if(n1.contains(num)) {
-               common.add(num);
+           if(common.contains(num)) {
+               return num;
            }
        }
 
-        if(common.size() == 0) {
-            return -1;
-        }
-
-        int min = Integer.MAX_VALUE;
-        for(int num : common) {
-            if(min > num) 
-                min = num;
-        }
-
-        return min;
+       return - 1;
     }
 }
