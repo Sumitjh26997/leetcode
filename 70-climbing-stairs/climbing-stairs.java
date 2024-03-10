d19d2e25862eb675eab5577235227fc39a13bbf2
Sumit Hotchandani <sumitjh@uw.edu>
2024-03-10 04:50:50 -0700

    Time: 0 ms (100.00%) | Memory: 39.9 MB (95.57%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,26 +1,17 @@
 class Solution {
-    HashMap<Integer, Integer> memo = new HashMap<>();
-    public int fib(int n) {
-        if(n==0){
-            return 0;
-        } 
-        if(n==1){
+    public int climbStairs(int n) {
+        if(n == 1) {
             return 1;
         }
-        if(n==2){
-            return 2;
+        int[] dp = new int[n+1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <=n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        if(memo.containsKey(n)) {
-            return memo.get(n);
-        }
-        int a = fib(n-2) + fib(n-1);
-        memo.put(n, a);
-
-        return a;
-    }
-   
-    public int climbStairs(int n) {
-        return fib(n);
+        return dp[n];
     }
 }
