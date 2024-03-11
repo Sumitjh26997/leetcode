cb531801b73a3a407316639a033f178d1913edb6
Sumit Hotchandani <sumitjh@uw.edu>
2024-03-11 15:31:14 -0700

    Time: 1 ms (93.45%) | Memory: 41.4 MB (20.41%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -1,33 +1,13 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        int count = 0;
-        int n = nums.length;
-        while(true) {
-            int min = Integer.MAX_VALUE;
-            int flag = 0;
+      HashSet<Integer> unique = new HashSet<>();
 
-            for(int i = 0; i < n; i++) {
-                if(nums[i] == 0) {
-                    flag++;
-                    continue;
-                }
-                if(nums[i] < min) {
-                    min = nums[i];
-                }
-            }
+      for(int num : nums) {
+          if(num > 0) {
+            unique.add(num);
+          }
+      }
 
-            if(flag == n) {
-                break;
-            }
-
-            for(int i = 0; i < n; i++) {
-                if(nums[i] != 0) {
-                    nums[i] -= min;
-                }
-            }
-            count++;
-        }
-
-        return count;
+      return unique.size();
     }   
 }
