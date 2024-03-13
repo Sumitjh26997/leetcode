a5b536bff5c85dc1f9f3fe1ea7fd199085d53147
Sumit Hotchandani <sumitjh@uw.edu>
2024-03-12 23:31:27 -0700

    Time: 20 ms (13.55%) | Memory: 41.9 MB (7.16%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int pivotInteger(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        int left = 1, right = n;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            int leftSum = (mid*(mid + 1)) / 2;
+            int rightSum = (n*(n+1)) / 2 - (mid*(mid - 1)) / 2;
+
+            System.out.println("mid : " + mid + " leftSum : " + leftSum + " rightSum:" + rightSum);
+
+            if(leftSum == rightSum) {
+                return mid;
+            } else if(leftSum < rightSum) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
