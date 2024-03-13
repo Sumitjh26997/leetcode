5ae07a0fe8bf5c255611ff388906bf1132c8d7d6
Sumit Hotchandani <sumitjh@uw.edu>
2024-03-13 12:41:45 -0700

    Time: 32 ms (63.63%) | Memory: 51.2 MB (41.38%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -10,17 +10,12 @@ class Solution {
             rowMap.put(row.toString(), rowMap.getOrDefault(row.toString(), 0) + 1);
         }
 
-        for(Map.Entry<String, Integer> entry : rowMap.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
-
         int pairs = 0;
         for(int j = 0; j < grid[0].length; j++) {
             StringBuilder column = new StringBuilder();
             for(int i = 0; i < grid.length; i++) {
                 column.append(grid[i][j]+',');
             }
-            System.out.println(column.toString());
             if(rowMap.containsKey(column.toString())) {
                 pairs += rowMap.get(column.toString());
             }
