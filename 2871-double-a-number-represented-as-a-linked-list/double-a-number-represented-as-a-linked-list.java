d6f8848ce12462ddbe3d63391539e9f188bd8d09
Sumit Hotchandani <sumitjh@uw.edu>
2024-05-06 18:41:07 -0700

    Time: 38 ms (8.59%) | Memory: 45.4 MB (91.41%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -6,37 +6,6 @@
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-
- long num = 0;
-
-        ListNode traversal = head;
-        while(traversal != null) {
-            num = (num * 10) + traversal.val;
-            traversal = traversal.next;
-        }
-
-        System.out.println(num);
-        num *= 2;
-        String result = String.valueOf(num);
-
-        ListNode newTraversal = new ListNode();
-        ListNode res = new ListNode();
-        res.next = newTraversal;
-        for(int i = 0; i < result.length(); i++) {
-            newTraversal.val = Character.getNumericValue(result.charAt(i));
-            System.out.println(newTraversal.val);
-            if(i != result.length() - 1) {
-                newTraversal.next = new ListNode();
-                newTraversal = newTraversal.next;
-            }
-        }
-
-        // while(res != null) {
-        //     System.out.println(res.val);
-        //     res = res.next;
-        // }
-        return res.next;
- * }
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
