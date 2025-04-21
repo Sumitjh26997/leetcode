02336b4328ae3458925457a80fe6c0103f469b41
Sumit Hotchandani <sumitjh@uw.edu>
2025-04-21 09:58:35 -0700

    Time: 0 ms (100.00%) | Memory: 44.4 MB (60.87%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -2,21 +2,18 @@ class Solution {
     public int diagonalSum(int[][] mat) {
         int sum = 0;
         int n = mat.length;
-        int sec = n-1;
 
         for(int i = 0; i < n; i++) {
             //even-odd check
             if(n%2 != 0 && i == n/2) {
                 // primary diagonal, skip secondary
                 sum += mat[i][i];
-                sec--;
                 continue;
             }
             // primary diagonal
             sum += mat[i][i];
             // secondary diagonal
-            sum += mat[i][sec];
-            sec--;
+            sum += mat[i][n-i-1];
         }
 
         return sum;
